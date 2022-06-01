a0eea3f8181a732f35b3de6e9a5a60e59362a3f1
Shuqri Shaifuddin <82794055+iirquhs-np@users.noreply.github.com>
2022-06-01 20:56:31 +0800

    Added bottom nav bar with fragments. Changed app icon.

--- app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
@@ -1,14 +1,48 @@
 package sg.edu.np.mad.socrata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import sg.edu.np.mad.socrata.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.nav_modules:
+                    replaceFragment(new ModuleFragment());
+                    break;
+                case R.id.nav_homework:
+                    replaceFragment(new HomeworkFragment());
+                    break;
+                case R.id.nav_profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
