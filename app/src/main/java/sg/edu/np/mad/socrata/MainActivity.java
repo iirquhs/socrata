a71c2f671d2f70e7e184107c183e49384869eb30
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2022-06-06 19:50:36 +0800

    Disabled night mode so background is white

--- app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
