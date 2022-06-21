b6f7085aaea3b6c0403c416f10d3badb92589cbc
Shuqri Shaifuddin <82794055+iirquhs-np@users.noreply.github.com>
2022-06-21 22:12:36 +0800

    Added delete account + loading spinners

--- app/src/main/java/sg/edu/np/mad/socrata/ProfileFragment.java
+++ app/src/main/java/sg/edu/np/mad/socrata/ProfileFragment.java
@@ -2,6 +2,9 @@ package sg.edu.np.mad.socrata;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,7 +19,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,9 +35,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
 
-        TextView signOutButton = fragmentView.findViewById(R.id.signOut);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // SIGN OUT FROM ACCOUNT
+        TextView signOutButton = getView().findViewById(R.id.signOutBtn);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,19 +57,13 @@ public class ProfileFragment extends Fragment {
         });
 
 
-        return fragmentView;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference user = FirebaseDatabase.getInstance().getReference("Users").child(uid);
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Users").child(uid);
 
+        // GET USERNAME STRING
         TextView profileUsername = getView().findViewById(R.id.profileUsername);
         TextView accountName = getView().findViewById(R.id.accountName);
-        DatabaseReference usernameRef = user.child("username");
+        DatabaseReference usernameRef = userRef.child("username");
         usernameRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -72,8 +79,9 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        // GET EMAIL STRING
         TextView profileEmail = getView().findViewById(R.id.profileEmail);
-        DatabaseReference emailRef = user.child("email");
+        DatabaseReference emailRef = userRef.child("email");
         emailRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -87,6 +95,46 @@ public class ProfileFragment extends Fragment {
                 Log.w(TAG, "loadEmail:onCancelled", databaseError.toException());
             }
         });
+
+        // DELETE ACCOUNT FROM DATABASE
+        TextView deleteAccBtn = getView().findViewById(R.id.deleteAccBtn);
+        deleteAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder
+                        .setTitle("PLEASE CONFIRM")
+                        .setMessage("Are you sure you want to delete your account?")
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ProgressDialog dialog = ProgressDialog.show(getActivity(), "Deleting your account", "Loading. Please wait...", true);
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.delete()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                    dialog.dismiss();
+                                                    startActivity(intent);
+                                                }
+                                            }
+                                        });
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
 
 
     }
