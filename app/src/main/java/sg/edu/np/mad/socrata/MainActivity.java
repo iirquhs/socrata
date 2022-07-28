f492939fdb01d1248ed202a821be222d16502619
Leo Yun Tao <39025657+LeoYunTao@users.noreply.github.com>
2022-07-29 03:15:01 +0800

    minor bug fixes

--- app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/socrata/MainActivity.java
@@ -76,10 +76,11 @@ public class MainActivity extends AppCompatActivity {
             MotivationalQuoteSetting motivationalQuoteSetting = new MotivationalQuoteSetting(true,
                     LocalTime.of(8,0), dateFrequencyList.get(0), 1);
             localStorage.setMotivationalQuoteSetting(motivationalQuoteSetting);
-
-            AlarmManagerHelper alarmManagerHelper = new AlarmManagerHelper(this);
-            alarmManagerHelper.setMotivationalSettingAlarm(motivationalQuoteSetting);
         }
+
+        MotivationalQuoteSetting motivationalQuoteSetting = localStorage.getMotivationalQuoteSetting();
+        AlarmManagerHelper alarmManagerHelper = new AlarmManagerHelper(this);
+        alarmManagerHelper.setMotivationalSettingAlarm(motivationalQuoteSetting);
 
         userUID = user.getUid();
 
